b2458cee0e799b4214ab0519b07ac8f5e89f01cb
Bajinder Singh <BajinderSingh@MacBook-Pro.local>
2016-08-02 16:05:41 +1000

    Common variables moved to Globals.ts

--- client/app/app.ts
+++ client/app/app.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
-import * as io from 'socket.io-client';
+import {Globals} from './globals';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -10,26 +10,16 @@ import * as io from 'socket.io-client';
 export class MyApp {
 
   private rootPage: any;
-  private socket: any;
+  public global: Globals;
   constructor(private platform: Platform) {
     this.rootPage = HomePage;
-    this.socket = io('http://localhost:3030');
+    this.global=new Globals();
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.socket.emit('news', 'this is news', function (data) {
-        console.log(data);
-
-      });
-      this.socket.on('broadcast', function (data) {
-        console.log("broadcasting revcieved " + data);
-      })
-      this.socket.on('pb', function (d) {
-        console.log('private: ' + d);
-      });
       StatusBar.styleDefault();
     });
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp,[Globals]);
